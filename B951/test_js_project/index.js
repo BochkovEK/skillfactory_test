9fec6538a3fcb105ab730d71fe169c4c17c5a737
BochkovEK <117360522+BochkovEK@users.noreply.github.com>
2024-01-17 17:48:49 +0300

    fix

---
+++ B951/test_js_project/index.js
@@ -0,0 +1,12 @@
+//import { multiplyByTwo, removeDuplicates } from "@bochkov-ek/array_app_js_test";
+
+const array_app_js_test = require('@bochkov-ek/array_app_js_test');
+
+const arr1 = ['1', '2', '3', '4'];
+const arr2 = ['banana', 'apple', 'peach', 'apple'];
+
+const output1 = array_app_js_test.multiplyByTwo(arr1);
+const output2 = array_app_js_test.removeDuplicates(arr2);
+
+console.log(output1);
+console.log(output2);
