ce54ccfdb9b6760745f715f4e8989e88776189df
BochkovEK <117360522+BochkovEK@users.noreply.github.com>
2024-01-17 17:41:57 +0300

    add node js test

---
+++ B951/test_app_array/test_app_array/array_app_js_test.js
@@ -0,0 +1,8 @@
+// module "array_app_js_test.js"
+exports.multiplyByTwo = function (array) {
+  return array.map((number) => number * 2);
+}
+
+exports.removeDuplicates = function (array) {
+  return [...new Set(array)]
+}
